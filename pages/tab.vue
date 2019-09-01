1c95c4ea92d57f0e802f6c320c6c6b652f5963dc
李小姐 <1121024033@qq.com>
2019-09-01 16:12:51 +0800

    闲云旅游项目初始化

---
+++ pages/tab.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="box">
+        <div class="tab">
+            <span v-for="(item,index) in ['新闻','体育','娱乐']" :key="index" @click="aaa(index)" :class="{color:current === index}">
+                {{item}}
+            </span>
+        </div>
+        <div v-show="current === 0">
+            <ul>
+                <li>1</li>
+                <li>1</li>
+                <li>1</li>
+            </ul>
+        </div>
+        <div v-show="current === 1">
+            <ul>
+                <li>2</li>
+                <li>2</li>
+                <li>2</li>
+            </ul>
+        </div>
+        <div>
+            <ul v-show="current === 2">
+                <li>3</li>
+                <li>3</li>
+                <li>3</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                // 记录当前的状态
+                current:0
+            }
+        },
+        methods: {
+        aaa(index){
+            // 修改current
+            this.current = index;
+        }
+    }
+    }
+</script>
+
+<style lang="less" scoped>
+.box{
+    padding: 50px;
+    margin-top: 20px;
+    .tab{
+        padding:0px 10px;
+        span{
+            cursor:pointer;
+        }
+    }
+    .color{
+        color: aquamarine
+    }
+}
+</style>
