5cf34a2c0a35a766f6bec47a1528883a9d824579
李小姐 <1121024033@qq.com>
2019-09-10 19:46:55 +0800

    添加乘机人，删除乘机人，在组件中渲染保险数据等完成

--- components/air/orderForm.vue
+++ components/air/orderForm.vue
@@ -1,186 +1,197 @@
 <template>
-    <div class="main">
-        <!-- 订单表单页 -->
-        <div class="air-column">
-            <h2>乘机人</h2>
-            <el-form class="member-info">
-                <div class="member-info-item" >
+  <div class="main">
+    <!-- 订单表单页 -->
+    <div class="air-column">
+      <h2>乘机人</h2>
+      <el-form class="member-info">
+        <div class="member-info-item" v-for="(item,index) in users" :key="index">
+          <el-form-item label="乘机人类型">
+            <el-input placeholder="姓名" class="input-with-select" v-model="users[index].username">
+              <!-- 下拉列表 -->
+              <el-select slot="prepend" value="1" placeholder="请选择">
+                <el-option label="成人" value="1"></el-option>
+              </el-select>
+            </el-input>
+          </el-form-item>
 
-                    <el-form-item label="乘机人类型">
-                        <el-input placeholder="姓名" class="input-with-select">
-                            <!-- 下拉列表 -->
-                            <el-select 
-                            slot="prepend" 
-                            value="1" 
-                            placeholder="请选择">
-                                <el-option label="成人" value="1"></el-option>
-                            </el-select>
-                        </el-input>
-                    </el-form-item>
-
-                    <el-form-item label="证件类型">
-                        <el-input 
-                        placeholder="证件号码"  class="input-with-select">
-                            <el-select 
-                            slot="prepend" 
-                            value="1"           
-                            placeholder="请选择">
-                                <el-option label="身份证" value="1" :checked="true"></el-option>
-                            </el-select>
-                        </el-input>
-                    </el-form-item>
-                    <!-- 添加多个乘机人时会显示 可点击删除 -->
-                    <span class="delete-user" @click="handleDeleteUser()">-</span>
-                </div>
-            </el-form>
-            <!-- 添加乘机人按钮 -->
-            <el-button class="add-member" type='primary' @click="handleAddUsers">添加乘机人</el-button>
+          <el-form-item label="证件类型">
+            <el-input placeholder="证件号码" class="input-with-select" v-model="users[index].id">
+              <el-select slot="prepend" value="1" placeholder="请选择">
+                <el-option label="身份证" value="1" :checked="true"></el-option>
+              </el-select>
+            </el-input>
+          </el-form-item>
+          <!-- 添加多个乘机人时会显示 可点击删除 -->
+          <span class="delete-user" @click="handleDeleteUser(index)">-</span>
         </div>
-
-        <div class="air-column">
-            <h2>保险</h2>
-            <div>
-                <div class="insurance-item">
-                    <el-checkbox 
-                    label="航空意外险：￥30/份×1  最高赔付260万" 
-                    border>
-                    </el-checkbox> 
-                </div>
-            </div>
-        </div>
-
-        <div class="air-column">
-            <h2>联系人</h2>
-            <div class="contact">
-                <el-form label-width="60px">
-                    <el-form-item label="姓名">
-                        <el-input></el-input>
-                    </el-form-item>
-
-                    <el-form-item label="手机">
-                        <el-input placeholder="请输入内容">
-                            <template slot="append">
-                            <el-button @click="handleSendCaptcha">发送验证码</el-button>
-                            </template>
-                        </el-input>
-                    </el-form-item>
-
-                    <el-form-item label="验证码">
-                        <el-input></el-input>
-                    </el-form-item>
-                </el-form>   
-                <el-button type="warning" class="submit" @click="handleSubmit">提交订单</el-button>
-            </div>
-        </div>
+      </el-form>
+      <!-- 添加乘机人按钮 -->
+      <el-button class="add-member" type="primary" @click="handleAddUsers">添加乘机人</el-button>
     </div>
+
+    <div class="air-column">
+      <h2>保险</h2>
+      <div>
+        <div class="insurance-item" v-for="(item,index) in data.insurances" :key="index">
+          <el-checkbox :label="`${item.type}：￥${item.price}/份×${users.length}  最高赔付${item.compensation}`" border></el-checkbox>
+        </div>
+      </div>
+    </div>
+
+    <div class="air-column">
+      <h2>联系人</h2>
+      <div class="contact">
+        <el-form label-width="60px">
+          <el-form-item label="姓名">
+            <el-input></el-input>
+          </el-form-item>
+
+          <el-form-item label="手机">
+            <el-input placeholder="请输入内容">
+              <template slot="append">
+                <el-button @click="handleSendCaptcha">发送验证码</el-button>
+              </template>
+            </el-input>
+          </el-form-item>
+
+          <el-form-item label="验证码">
+            <el-input></el-input>
+          </el-form-item>
+        </el-form>
+        <el-button type="warning" class="submit" @click="handleSubmit">提交订单</el-button>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-    methods: {
-        // 添加乘机人
-        handleAddUsers(){
-            
-        },
-        
-        // 移除乘机人
-        handleDeleteUse(){
-
-        },
-        
-        // 发送手机验证码
-        handleSendCaptcha(){
-            
-        },
-
-        // 提交订单
-        handleSubmit(){
-            
+  // 接收父组件传过来的数据
+  props:{
+       // 接收机票信息
+       data:{
+           type:Object,
+           default:{}
+       }   
+  },
+  data() {
+    return {
+      // 乘机人用户数据=>看文档得知需要传的是数组对象
+      users: [
+        {
+          username: "",
+          id: ""
         }
-    }
-}
+      ],
+      // 是否购买保险 => 需要id，是一个数组(集合id)=>父组件中已获取
+      // insurances:[]
+    };
+  },
+  methods: {
+    // 添加乘机人
+    handleAddUsers() {
+        // 添加多一个列表
+        this.users.push({
+            username: "",
+            id: ""
+        })
+    },
+
+    // 移除乘机人
+    handleDeleteUser(index) {
+        // 把users里的某一项移除 => 数组的方法splice(从第几个开始删除,删除几个[,插入项(多个用数组写)])
+        this.users.splice(index,1)
+    },
+
+    // 发送手机验证码
+    handleSendCaptcha() {},
+
+    // 提交订单
+    handleSubmit() {}
+  }
+};
 </script>
 
 <style scoped lang="less">
-    .air-column{
-        border-bottom:1px #ddd dashed;
-        padding-bottom: 20px;   
-        margin-bottom: 20px;
-    }
+.air-column {
+  border-bottom: 1px #ddd dashed;
+  padding-bottom: 20px;
+  margin-bottom: 20px;
+}
 
-    .air-column h2{
-        margin-bottom: 20px;
-        font-size: 22px;
-        font-weight: normal;
-    }
+.air-column h2 {
+  margin-bottom: 20px;
+  font-size: 22px;
+  font-weight: normal;
+}
 
-    /deep/ .el-select .el-input {
-        width: 130px;
-    }
+/deep/ .el-select .el-input {
+  width: 130px;
+}
 
-    .input-with-select{
-        width:590px;
-    }
+.input-with-select {
+  width: 590px;
+}
 
-    .input-with-select /deep/  .el-input-group__prepend {
-        background-color: #fff;
-    }
-    .member-info /deep/ .el-form-item{
-        margin-bottom:0;
-    }
+.input-with-select /deep/ .el-input-group__prepend {
+  background-color: #fff;
+}
+.member-info /deep/ .el-form-item {
+  margin-bottom: 0;
+}
 
-    .member-info-item{
-        border-bottom:1px #eee dashed;
-        padding-bottom: 20px;
-        position: relative;
+.member-info-item {
+  border-bottom: 1px #eee dashed;
+  padding-bottom: 20px;
+  position: relative;
 
-        &:first-child{
-            // 删除按钮通过样式控制=>第一个列表不显示
-            .delete-user{
-                display: none;
-            }
-        }
+  &:first-child {
+    // 删除按钮通过样式控制=>第一个列表不显示
+    .delete-user {
+      display: none;
     }
+  }
+}
 
-    .add-member{
-        margin-top:20px;
-    }
+.add-member {
+  margin-top: 20px;
+}
 
-    .delete-user{
-        display: block;
-        background:#ddd;
-        width:16px;
-        height:16px;
-        font-size:14px;
-        text-align: center;
-        line-height: 16px;
-        color:#fff;
-        cursor: pointer;
-        border-radius: 50px;
-        position:absolute;
-        right:-30px;
-        top:50%;
-    }
+.delete-user {
+  display: block;
+  background: #ddd;
+  width: 16px;
+  height: 16px;
+  font-size: 14px;
+  text-align: center;
+  line-height: 16px;
+  color: #fff;
+  cursor: pointer;
+  border-radius: 50px;
+  position: absolute;
+  right: -30px;
+  top: 50%;
+}
 
-    .insurance{
-        > div{
-            margin-top:10px;
-        }
-    }
+.insurance {
+  > div {
+    margin-top: 10px;
+  }
+}
 
-    .insurance-item{
-        margin-bottom: 20px;
-    }
+.insurance-item {
+  margin-bottom: 20px;
+}
 
-    .contact{
-        /deep/ .el-input{
-            width:50%;
-        }
-    }
+.contact {
+  /deep/ .el-input {
+    width: 50%;
+  }
+}
 
-    .submit{
-        margin: 50px auto;
-        display: block;
-        width:250px;
-        height:50px;
-    }
+.submit {
+  margin: 50px auto;
+  display: block;
+  width: 250px;
+  height: 50px;
+}
 </style>
--- pages/air/order.vue
+++ pages/air/order.vue
@@ -3,7 +3,8 @@
         <el-row type="flex" justify="space-between">
             <!-- 订单表单 -->
             <div class="main">
-                <orderForm/>
+                <!-- 父组件接口传值=>把机票数据传递到子组件 -->
+                <orderForm :data="infoData"/>
             </div>
 
             <!-- 侧边栏 -->
@@ -20,6 +21,29 @@ import orderForm from '@/components/air/orderForm.vue'
 import orderAside from '@/components/air/orderAside.vue'
 
 export default {
+    data(){
+        return{
+            // 机票的数据
+            infoData:{
+                // 保险id的集合=>数组=>初始化
+                insurances:[]
+            }
+        }
+    },
+    // 事件处理
+    mounted(){
+        // 把选择机票跳转需要携带的参数解构出来=>获取路由携带的参数用query（$route）=>带r($router)是路由跳转
+        const {id,seat_xid} = this.$route.query;
+        // 请求机票数据
+        this.$axios({
+            url: "/airs/" + id, // 或者可以这样写 `/airs/${id}`
+            params:{ seat_xid }
+        }).then(res=>{
+            // 保存机票的数据
+            this.infoData = res.data
+            console.log(this.infoData);
+        })
+    },
     // 注册组件
     components:{
         // 表单组件
